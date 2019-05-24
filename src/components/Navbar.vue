1c31435a433f9bd808cb43fd21a1f26bfe0afab4
alexlibe95 <alexlibe95@gmail.com>
2019-05-24 03:21:21 +0300

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+
+<header>
+      <nav class="navbar navbar-expand-md custom-nav-bg navbar-light border-bottom shadow-sm">
+        <a class="my-0 mr-md-auto navbar-brand text-dark" href="#">
+       <img class="nav-logo" alt="Vue logo" src="../assets/logo.png"/> Task Manager
+         </a> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tasks</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>           
+            <li>
+            <a class="btn btn-outline-primary" href="#">Sign in</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+</template>
